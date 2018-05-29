6929f970cf1f612f3b8965e6fa05118056d73d4e
Jacob Martinez <jacobmartinez3d@gmail.com>
2018-05-29 11:25:54 -0700

    flask and node now commnicating with eachother. Time to build out nuke tools..

--- app.py
+++ app.py
@@ -1,22 +1,61 @@
-import sys
-
+import sys, os, random, json
+import opentimelineio as otio
+from shutil import copyfile
 from flask import Flask, render_template, request, redirect, Response
-import random, json
-
+dir_path = os.path.dirname(os.path.realpath(__file__))
 app = Flask(__name__)
 
 @app.route('/')
 def output():
-	# serve index template
-	return
+	print("Index has been pinged!")
+	return "C1 Python Endpoint"
 
-@app.route('/convert_to_otio', methods = ['POST'])
-def worker():
+@app.route('/sync_otio', methods = ['POST'])
+def sync_otio():
+	def writeOtioFile(filepath):
+		timeline_name = None
+		operation = None
+		try:
+			timeline_name = os.path.basename(filepath).split(".")[0]
+			fileExtension = os.path.basename(filepath).split(".")[1]
+		except:
+			print("Filetype not recognized.")
+			return False
+		save_to = os.path.join(os.path.dirname(filepath), timeline_name)
+		try:
+			timeline = otio.adapters.read_from_file(filepath)
+		except:
+			print("No Adapter for this filetype.\nSee available Adapters here:\nhttps://github.com/PixarAnimationStudios/OpenTimelineIO\#adapters")
+			return False
+		if fileExtension == "xml":
+			# create .otio and .json save paths
+			save_otio_to = save_to + ".otio"
+			save_json_to = save_to + ".json"
+			# generate .otio
+			otio.adapters.write_to_file(timeline, save_otio_to)
+			# copy the .otio file and save as .json (for REACT app)
+			copyfile(save_otio_to, save_json_to)
+		elif fileExtension == "otio":
+			save_to += ".xml"
+			otio.adapters.write_to_file(timeline, save_to)
+		print("Saved to:\n" + save_to)
+		data = {otio: timeline, path : {json:save_json_to, otio: save_otio_to}}
+		return data
+	def writeShotLog(timeline):
+		print timeline
+		for each_seq in timeline.tracks:
+			for each_item in each_seq:
+				if isinstance(each_item, otio.schema.Clip):
+					print each_item.media_reference
+
+	#########################################################
 	# read json + reply
 	data = request.get_json()
-	result = ''
-
-	return result
+	newPath = os.path.join(dir_path, "src", "OTIO", os.path.basename(data["fcpxml"]))
+	timeline = writeOtioFile(newPath)
+	print("sync_otio has been pinged!\n\n" + str(timeline))
+	writeShotLog(timeline)
+	return "synced"
 
 if __name__ == '__main__':
 	# run!
--- src/App.js
+++ src/App.js
@@ -1,73 +1,112 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Button, Table } from "reactstrap";
-import { CSVLink, CSVDownload } from "react-csv";
-const otio_json = require("G:/Users/Jacob/c1_otio/src/CMP_testTimeline.json");
+import { Table } from "reactstrap";
+// import { CSVLink, CSVDownload } from "react-csv";
+const fcpxml = "./OTIO/CMP_testTimeline.xml";
+var sync_fcpxml, otio_json;
+try {
+  sync_fcpxml = require("./OTIO/CMP_testTimeline.xml");
+  otio_json = require("./OTIO/CMP_testTimeline.json");
+} catch (err) {
+  sync_fcpxml = "jacob";
+  otio_json = "rulz";
+}
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       button_label: "Build Spreadsheet",
-      timeline: otio_json,
-      shotLog: null
+      timeline: null,
+      shotLog: null,
+      fcpxml: fcpxml,
+      // set state to the required file to detect changes (this is the beauty of react!)
+      sync_fcpxml: sync_fcpxml || "jacob"
     };
   }
   componentDidMount() {
+    console.log("RESULT from flask:", this.syncTimeline(this.state.fcpxml));
+    this.setState({
+      shotLog: this.createShotLog()
+    });
+  }
+  syncTimeline(fcpxml) {
+    var data = JSON.stringify({ fcpxml: fcpxml });
+    var synced_otio = fetch("http://127.0.0.1:5000/sync_otio", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        mode: "no-cors"
+        // "Content-Length": data.length
+      },
+      body: data
+    })
+      // What are all these for?
+      .then(response => {
+        return response;
+      })
+      .then(response => response.ok && response.json())
+      .then(responseJson => {})
+      .catch(error => {});
+    return synced_otio;
+  }
+  createShotLog() {
     const csvData = [];
     // Tracks
-    otio_json.tracks.children.forEach((track, i) => {
-      if (track.kind == "Video") {
-        console.warn(track);
-        // Video Clips
-        track.children.forEach((clip, j) => {
-          if (clip.OTIO_SCHEMA == "Clip.1") {
-            console.warn(clip);
-            csvData.push([
-              // 0: Name
-              clip.name.split(".")[0],
-              // 1: Source Duration
-              clip.media_reference.available_range.duration.value,
-              // 2: Edit Duration
-              clip.source_range.duration.value,
-              // 3: Edit In
-              clip.source_range.start_time.value,
-              // 4: Edit Out
-              clip.source_range.start_time.value + clip.source_range.duration.value,
-              // 5: VFX Notes
-              clip.markers.map((marker, k) => {
-                if (marker.name && marker.marked_range.duration.value < 1) {
-                  console.warn(marker);
-                  return "x" + marker.marked_range.start_time.value + ", ";
-                } else if (marker.name && marker.marked_range.duration.value >= 1) {
-                  console.warn(marker);
-                  return (
-                    "[VFX]: x" +
-                    marker.marked_range.start_time.value +
-                    " - x" +
-                    marker.marked_range.start_time.value +
-                    marker.marked_range.duration.value +
-                    ", "
-                  );
-                }
-              }),
-              // 6:
-              clip.media_reference.target_url
-            ]);
-          }
-        });
-      }
-    });
-    this.setState({
-      shotLog: csvData
-    });
-    console.log(csvData);
+    try {
+      otio_json.tracks.children.forEach((track, i) => {
+        if (track.kind === "Video") {
+          // console.warn(track);
+          // Video Clips
+          track.children.forEach((clip, j) => {
+            if (clip.OTIO_SCHEMA === "Clip.1") {
+              // console.warn(clip);
+              csvData.push([
+                // 0: Name
+                clip.name.split(".")[0],
+                // 1: Source Duration
+                clip.media_reference.available_range.duration.value,
+                // 2: Edit Duration
+                clip.source_range.duration.value,
+                // 3: Edit In
+                clip.source_range.start_time.value,
+                // 4: Edit Out
+                clip.source_range.start_time.value + clip.source_range.duration.value,
+                // 5: VFX Notes
+                clip.markers.map((marker, k) => {
+                  var result;
+                  if (marker.name && marker.marked_range.duration.value < 1) {
+                    // console.warn(marker);
+                    result = "x" + marker.marked_range.start_time.value + ", ";
+                  } else if (marker.name && marker.marked_range.duration.value >= 1) {
+                    // console.warn(marker);
+                    result =
+                      "[VFX]: x" +
+                      marker.marked_range.start_time.value +
+                      " - x" +
+                      marker.marked_range.start_time.value +
+                      marker.marked_range.duration.value +
+                      ", ";
+                  }
+                  return result;
+                }),
+                // 6: URL
+                clip.media_reference.target_url
+              ]);
+            }
+          });
+        }
+      });
+    } catch (err) {
+      console.warn(err);
+    }
+    return csvData;
   }
-
   render() {
     if (!this.state.shotLog) {
-      return <h1>Loading TImeline...</h1>;
+      return <h1>Loading Timeline...</h1>;
     }
     return (
       <div className="App">
@@ -75,6 +114,9 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">C1 Timeline</h1>
         </header>
+        <p>
+          {this.state.fcpxml && this.state.fcpxml}
+        </p>
 
         {this.state.shotLog &&
           <Table>
@@ -89,34 +131,35 @@ class App extends React.Component {
                 <th>Source Url</th>
               </tr>
             </thead>
-            {this.state.shotLog.map((shot, i) => {
-              return (
-                <tr>
-                  <th>
-                    {shot[0]}
-                  </th>
-                  <th>
-                    {shot[1]}
-                  </th>
-                  <th>
-                    {shot[2]}
-                  </th>
-                  <th>
-                    {shot[3]}
-                  </th>
-                  <th>
-                    {shot[4]}
-                  </th>
-                  <th>
-                    {shot[5]}
-                  </th>
-                  <th>
-                    {shot[6]}
-                  </th>
-                </tr>
-              );
-            })}
-            <tbody />
+            <tbody>
+              {this.state.shotLog.map((shot, i) => {
+                return (
+                  <tr key={i}>
+                    <th>
+                      {shot[0]}
+                    </th>
+                    <th>
+                      {shot[1]}
+                    </th>
+                    <th>
+                      {shot[2]}
+                    </th>
+                    <th>
+                      {shot[3]}
+                    </th>
+                    <th>
+                      {shot[4]}
+                    </th>
+                    <th>
+                      {shot[5]}
+                    </th>
+                    <th>
+                      {shot[6]}
+                    </th>
+                  </tr>
+                );
+              })}
+            </tbody>
           </Table>}
       </div>
     );
